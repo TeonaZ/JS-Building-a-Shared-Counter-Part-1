6fc5bd62cef9e0cacaf3887d0601645f97266523
Teona <teonazaalishvili2@gmail.com>
2023-01-08 11:31:06 -0500

    add code

--- public/index.js
+++ public/index.js
@@ -1,9 +1,12 @@
-function main(){
+async function main(){
     const countContainer = document.querySelector('#count-container');
     const incrementButton = document.querySelector('#increment-button');
     const decrementButton = document.querySelector('#decrement-button');
 
-    let countValue = 0;
+    let response = await fetch('http://127.0.0.1:9001/counter')
+    let result = await response.json()
+    console.log(result)
+    let countValue = result.value;
 
     function increment(){
         countValue++;
